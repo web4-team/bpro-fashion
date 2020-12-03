78d925fbfd8fed9b92d91026b5c37f9b68a77939
Htet Myet Lwin <htetmyetlwin@ritz.com.mm>
2020-12-03 21:43:35 +0630

    Hello Web4git status!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Web 4!!!
                 </div>
 
                 <div class="links">
