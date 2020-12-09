31c23392d48c37bd129ae1e66cf2aef233d001fb
Htet Myet Lwin <htetmyetlwin@ritz.com.mm>
2020-12-10 00:42:50 +0630

    added constructor middleware in CourseController

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -7,6 +7,16 @@ use App\Course;
 
 class CourseController extends Controller
 {
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
