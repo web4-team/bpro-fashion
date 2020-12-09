439bf76587159a765f329384e6711ca8bfc13787
Htet Myet Lwin <htetmyetlwin@ritz.com.mm>
2020-12-09 15:15:54 +0630

    added user update fields in edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -19,19 +19,49 @@
           </div>
           <div class="card-body">
           <form action="{{ route('admin.users.update', $user)}}" method="POST">
+            <div class="form-group row">
+              <label for="name" class="col-md-2 col-form-label text-md-right">Name</label>
+  
+              <div class="col-md-6">
+                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autofocus>
+  
+                  @error('name')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+              </div>
+          </div>
+           
+            <div class="form-group row">
+              <label for="email" class="col-md-2 col-form-label text-md-right">Email</label>
+
+              <div class="col-md-6">
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+
+                  @error('email')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+              </div>
+          </div>
+
             @csrf
             {{ method_field('PUT')}}
+            <div class="form-group row">
+              <label for="roles" class="col-md-2 col-form-label text-md-right">Roles</label>
+              <div class="col-md-6">
             @foreach($roles as $role)
-           
-               
-             
                   <div class="form-checkbox">
-                  <input type="checkbox" name="roles[]" value="{{ $role->id}}">
+                  <input type="checkbox" name="roles[]" value="{{ $role->id}}"
+                  @if($user->roles->pluck('id')->contains($role->id)) checked @endif>
                   <label for="userrole">{{ $role->name}}</label>
                   </div>
               
             @endforeach
-                     
+              </div>
+            </div>       
               <div class="form-group row">
                 <div class="col-sm-10">
                   <button type="submit" class="btn btn-success">Update</button>
