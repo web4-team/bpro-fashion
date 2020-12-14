c95ce547ca81c7b6e5fea4837fae9053289a582b
Htet Myet Lwin <htetmyetlwin@ritz.com.mm>
2020-12-14 21:13:33 +0630

    removed component config/app.php

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Yajra\Datatables\DatatablesServiceProvider::class,
 
     ],
 
@@ -227,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Datatables' => Yajra\Datatables\Facades\Datatables::class,
 
     ],
 
