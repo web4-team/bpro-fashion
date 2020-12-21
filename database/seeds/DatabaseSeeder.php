419a4b619de6c1a18a671b8a6ab16902e8bb37bb
Htet Myet Lwin <htetmyetlwin10@gmail.com>
2020-12-21 14:44:45 +0630

    solved foreign error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::unguard();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         
     }
 }
