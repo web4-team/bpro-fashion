6c6f87b3cefd58f00c24cbf07603e01b226cc137
Htet Myet Lwin <htetmyetlwin@ritz.com.mm>
2021-01-16 12:00:25 +0630

    solved student conflict

--- app/Student.php
+++ app/Student.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-<<<<<<< HEAD
     protected $fillable=['course_id','batch_id', 'accept_date', 'name', 'dob', 'age', 'phone', 'email', 'education', 'address', 'objective', 'bpro','note'];
 
     public function course()
@@ -21,20 +20,4 @@ class Student extends Model
 
     }
 
-=======
-    protected $fillable=['course_id', 'accept_date', 'name', 'dob', 'age', 'phone', 'email', 'education', 'address', 'objective', 'bpro'];
-
-public function courses($value='')
-    {
-    	return $this->belongsTo('App\Course')
-    				->withTimestamps();
-    }
-
-    public function batches($value='')
-    {
-    	return $this->belongsTo('App\Batch')
-    				->withTimestamps();
-    }
- 
->>>>>>> ppz
 }
