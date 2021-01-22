e827107c3d0979e2d5128fca4946495d5f134cc7
Htet Myet Lwin <htetmyetlwin@ritz.com.mm>
2021-01-22 20:32:47 +0630

    added edit.blade.php

--- resources/views/school_Mgm/edit.blade.php
+++ resources/views/school_Mgm/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row">
  <div class="col-sm-8 offset-sm-2">
-  <h1 class="display-3">Update Course</h1>
+  <h1 class="display-3">Update Batch</h1>
   <div>
     @if ($errors->any())
     <div class="alert alert-danger">
