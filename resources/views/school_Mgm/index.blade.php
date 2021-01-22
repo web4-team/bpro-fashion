1f16430a0e670e85a0c0bc2cc41e68fcf27bf5fd
pyaezaw97 <pyaephyozaw101@gmail.com>
2021-01-22 20:42:05 +0630

    friday

--- resources/views/school_Mgm/index.blade.php
+++ resources/views/school_Mgm/index.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.master')
-<<<<<<< HEAD
-@section('style')
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
-  <!-- Custom styles for this page -->
-  <link href="{{asset('backend/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="{{ asset('backend/js/demo/custom.css') }}">
-@endsection
-=======
->>>>>>> master
+
 @section('content')
 <!-- <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
 <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
